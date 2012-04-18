365dd7feddda877082697403be90618c03d09b15
Hugo S <hugo.flugsvamp@gmail.com>
2012-04-18 16:56:43 +0200

    Separated entity creation and adding from Game.init to Game.setupEntities

--- src/indaprojekt/Game.java
+++ src/indaprojekt/Game.java
@@ -33,6 +33,86 @@ public class Game extends BasicGame
     {
         super("Awesome Game");
     }
+    
+    /**
+     * Sets up and adds all the entities of the map to the game
+     */
+    private void setupEntities() throws SlickException
+    {	
+		PlayerControls player1Controls = new PlayerControls(
+										Input.KEY_W,
+										Input.KEY_A,
+										Input.KEY_S,
+										Input.KEY_D,
+										Input.KEY_LCONTROL);
+		Image player1Image = new Image("res//player.png");
+		Map<Direction, Animation> animMap1 = new HashMap<Direction, Animation>();
+		animMap1.put(Direction.UP, new Animation(new Image[]{player1Image}, 1));
+		animMap1.put(Direction.UPRIGHT, new Animation(new Image[]{player1Image}, 1));
+		animMap1.put(Direction.RIGHT, new Animation(new Image[]{player1Image}, 1));
+		animMap1.put(Direction.DOWNRIGHT, new Animation(new Image[]{player1Image}, 1));
+		animMap1.put(Direction.DOWN, new Animation(new Image[]{player1Image}, 1));
+		animMap1.put(Direction.DOWNLEFT, new Animation(new Image[]{player1Image}, 1));
+		animMap1.put(Direction.LEFT, new Animation(new Image[]{player1Image}, 1));
+		animMap1.put(Direction.UPLEFT, new Animation(new Image[]{player1Image}, 1));
+		Rectangle2D.Float player1HitBox = new Rectangle2D.Float(0f, 0f, 
+		player1Image.getWidth()/10f, player1Image.getHeight()/10f);
+		Player p1 = new Player(50, 50, player1Controls, player1HitBox, animMap1);
+		players.add(p1);
+		entities.add(p1);
+		
+		
+		//TEMP
+		PlayerControls player2Controls = new PlayerControls(
+						Input.KEY_UP,
+						Input.KEY_LEFT,
+						Input.KEY_DOWN,
+						Input.KEY_RIGHT,
+						Input.KEY_L);
+		Image player2Image = new Image("res//player2.png");
+		Map<Direction, Animation> animMap2 = new HashMap<Direction, Animation>();
+		animMap2.put(Direction.UP, new Animation(new Image[]{player2Image}, 1));
+		animMap2.put(Direction.UPRIGHT, new Animation(new Image[]{player2Image}, 1));
+		animMap2.put(Direction.RIGHT, new Animation(new Image[]{player2Image}, 1));
+		animMap2.put(Direction.DOWNRIGHT, new Animation(new Image[]{player2Image}, 1));
+		animMap2.put(Direction.DOWN, new Animation(new Image[]{player1Image}, 1));
+		animMap2.put(Direction.DOWNLEFT, new Animation(new Image[]{player2Image}, 1));
+		animMap2.put(Direction.LEFT, new Animation(new Image[]{player2Image}, 1));
+		animMap2.put(Direction.UPLEFT, new Animation(new Image[]{player2Image}, 1));
+		Rectangle2D.Float player2HitBox = new Rectangle2D.Float(0f, 0f, 
+		player2Image.getWidth()/10f, player2Image.getHeight()/10f);
+		Player p2 = new Player(150, 150, player2Controls, player2HitBox, animMap2);
+		players.add(p2);
+		entities.add(p2);
+		background = new Image("res//classroom.jpg");
+		
+		Image obstacleImage = new Image("res//bomb2.png");
+		Obstacle obstacle = new Obstacle(100, 100, new Rectangle2D.Float(0, 0, obstacleImage.getWidth(), obstacleImage.getHeight()), 
+		new Animation(new Image[]{obstacleImage}, 1));
+		obstacles.add(obstacle);
+		entities.add(obstacle);
+		
+		Obstacle leftWall = new Obstacle(0, 0, new Rectangle2D.Float(0, 0, 50, 600), new Animation(new Image[]{obstacleImage}, 1));
+		Obstacle rightWall = new Obstacle(750, 0, new Rectangle2D.Float(0, 0, 50, 600), new Animation(new Image[]{obstacleImage}, 1));
+		Obstacle bottomWall = new Obstacle(0, 550, new Rectangle2D.Float(0, 0, 800, 50), new Animation(new Image[]{obstacleImage}, 1));
+		Obstacle topWall = new Obstacle(0, 0, new Rectangle2D.Float(0, 0, 800, 50), new Animation(new Image[]{obstacleImage}, 1));
+		obstacles.add(leftWall);
+		entities.add(leftWall);
+		obstacles.add(rightWall);
+		entities.add(rightWall);
+		obstacles.add(bottomWall);
+		entities.add(bottomWall);
+		obstacles.add(topWall);
+		entities.add(topWall);
+		
+		Image itemImage = new Image("res//bomb2.png");
+		Item item = new Item(itemImage, 250, 250, new Rectangle2D.Float(0, 0, 25, 25));
+		entities.add(item);
+		
+		Image spdUpImage = new Image("res//Speed.png");
+		SpeedUp spdUp = new SpeedUp(spdUpImage, 300, 300, new Rectangle2D.Float(0,0,25,25), 2);
+		entities.add(spdUp);
+    }
  
     @Override
     public void init(GameContainer gc) throws SlickException 
@@ -42,80 +122,7 @@ public class Game extends BasicGame
     	obstacles = new ArrayList<Obstacle>();
     	entities = new LinkedList<Entity>();
     	
-    	//TEMP.
-    	PlayerControls player1Controls = new PlayerControls(
-											Input.KEY_W,
-											Input.KEY_A,
-											Input.KEY_S,
-											Input.KEY_D,
-											Input.KEY_LCONTROL);
-    	Image player1Image = new Image("res//player.png");
-    	Map<Direction, Animation> animMap1 = new HashMap<Direction, Animation>();
-    	animMap1.put(Direction.UP, new Animation(new Image[]{player1Image}, 1));
-    	animMap1.put(Direction.UPRIGHT, new Animation(new Image[]{player1Image}, 1));
-    	animMap1.put(Direction.RIGHT, new Animation(new Image[]{player1Image}, 1));
-    	animMap1.put(Direction.DOWNRIGHT, new Animation(new Image[]{player1Image}, 1));
-    	animMap1.put(Direction.DOWN, new Animation(new Image[]{player1Image}, 1));
-    	animMap1.put(Direction.DOWNLEFT, new Animation(new Image[]{player1Image}, 1));
-    	animMap1.put(Direction.LEFT, new Animation(new Image[]{player1Image}, 1));
-    	animMap1.put(Direction.UPLEFT, new Animation(new Image[]{player1Image}, 1));
-    	Rectangle2D.Float player1HitBox = new Rectangle2D.Float(0f, 0f, 
-    						player1Image.getWidth()/10f, player1Image.getHeight()/10f);
-    	Player p1 = new Player(50, 50, player1Controls, player1HitBox, animMap1);
-    	players.add(p1);
-    	entities.add(p1);
-    	
-    	
-    	//TEMP
-    	PlayerControls player2Controls = new PlayerControls(
-    										Input.KEY_UP,
-    										Input.KEY_LEFT,
-    										Input.KEY_DOWN,
-    										Input.KEY_RIGHT,
-    										Input.KEY_L);
-    	Image player2Image = new Image("res//player2.png");
-    	Map<Direction, Animation> animMap2 = new HashMap<Direction, Animation>();
-    	animMap2.put(Direction.UP, new Animation(new Image[]{player2Image}, 1));
-    	animMap2.put(Direction.UPRIGHT, new Animation(new Image[]{player2Image}, 1));
-    	animMap2.put(Direction.RIGHT, new Animation(new Image[]{player2Image}, 1));
-    	animMap2.put(Direction.DOWNRIGHT, new Animation(new Image[]{player2Image}, 1));
-    	animMap2.put(Direction.DOWN, new Animation(new Image[]{player1Image}, 1));
-    	animMap2.put(Direction.DOWNLEFT, new Animation(new Image[]{player2Image}, 1));
-    	animMap2.put(Direction.LEFT, new Animation(new Image[]{player2Image}, 1));
-    	animMap2.put(Direction.UPLEFT, new Animation(new Image[]{player2Image}, 1));
-    	Rectangle2D.Float player2HitBox = new Rectangle2D.Float(0f, 0f, 
-				player2Image.getWidth()/10f, player2Image.getHeight()/10f);
-    	Player p2 = new Player(150, 150, player2Controls, player2HitBox, animMap2);
-    	players.add(p2);
-    	entities.add(p2);
-    	background = new Image("res//classroom.jpg");
-    	
-    	Image obstacleImage = new Image("res//bomb2.png");
-    	Obstacle obstacle = new Obstacle(100, 100, new Rectangle2D.Float(0, 0, obstacleImage.getWidth(), obstacleImage.getHeight()), 
-    			new Animation(new Image[]{obstacleImage}, 1));
-    	obstacles.add(obstacle);
-    	entities.add(obstacle);
-    	
-    	Obstacle leftWall = new Obstacle(0, 0, new Rectangle2D.Float(0, 0, 50, 600), new Animation(new Image[]{obstacleImage}, 1));
-    	Obstacle rightWall = new Obstacle(750, 0, new Rectangle2D.Float(0, 0, 50, 600), new Animation(new Image[]{obstacleImage}, 1));
-    	Obstacle bottomWall = new Obstacle(0, 550, new Rectangle2D.Float(0, 0, 800, 50), new Animation(new Image[]{obstacleImage}, 1));
-    	Obstacle topWall = new Obstacle(0, 0, new Rectangle2D.Float(0, 0, 800, 50), new Animation(new Image[]{obstacleImage}, 1));
-    	obstacles.add(leftWall);
-    	entities.add(leftWall);
-    	obstacles.add(rightWall);
-    	entities.add(rightWall);
-    	obstacles.add(bottomWall);
-    	entities.add(bottomWall);
-    	obstacles.add(topWall);
-    	entities.add(topWall);
-    	
-    	Image itemImage = new Image("res//bomb2.png");
-    	Item item = new Item(itemImage, 250, 250, new Rectangle2D.Float(0, 0, 25, 25));
-    	entities.add(item);
-    	
-    	Image spdUpImage = new Image("res//Speed.png");
-    	SpeedUp spdUp = new SpeedUp(spdUpImage, 300, 300, new Rectangle2D.Float(0,0,25,25), 2);
-    	entities.add(spdUp);
+    	setupEntities();
     	
     	input = gc.getInput();
     }
