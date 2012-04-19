b3695edc388742b5892af078e864e50e47c3ba38
Hugo S <hugo.flugsvamp@gmail.com>
2012-04-19 16:03:17 +0200

    Players only move their feet when running

--- src/indaprojekt/Game.java
+++ src/indaprojekt/Game.java
@@ -231,7 +231,7 @@ public class Game extends BasicGame
     }
  
     public static void main(String[] args) throws SlickException
-    {
+    {	
          AppGameContainer app = new AppGameContainer(new Game());
          app.setDisplayMode(1000, 600, false);
          app.setVSync(true);
