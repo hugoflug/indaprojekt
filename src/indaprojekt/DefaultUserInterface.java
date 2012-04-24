7a4973a47ef58d015b3187e791ceecd132d3d99f
garlic <katja7667@gmail.com>
2012-04-24 17:28:46 +0200

    Moved more hearts

--- src/indaprojekt/DefaultUserInterface.java
+++ src/indaprojekt/DefaultUserInterface.java
@@ -9,7 +9,7 @@ public class DefaultUserInterface extends UserInterface {
 	public DefaultUserInterface(int screenWidth, int screenHeight)
 			throws SlickException {
 		
-		super(55, 55, screenWidth - 290, screenHeight - 40, 
+		super(58, 10, screenWidth - 290, screenHeight - 40, 
 				new Image("res//images//BlueHeart.png"), 
 				new Image("res//images//brokenHeart.png"), 
 				new Image("res//images//OrangeHeart.png"), 
