e7e3c9e831590bb153fda90d38d3d2897e54d250
Hugo S <hugo.flugsvamp@gmail.com>
2012-04-24 16:16:24 +0200

    Refactored friction effect into new ConstantMover class

---
+++ src/indaprojekt/ConstantMover.java
@@ -0,0 +1,31 @@
+package indaprojekt;
+
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Float;
+
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+
+public abstract class ConstantMover extends Mover
+{
+	protected float dx, dy;
+	protected float friction;
+
+	public ConstantMover(float x, float y, Rectangle2D.Float hitBox, float initialDX, float initialDY,
+						 float friction) 
+	{
+		super(x, y, hitBox);
+		dx = initialDX;
+		dy = initialDY;
+		this.friction = friction;
+	}
+
+	@Override
+	public void doLogic(Input input, int delta) throws SlickException
+	{
+		move(dx*delta, dy*delta);
+		dx = General.towardsZero(dx, friction);
+		dy = General.towardsZero(dy, friction);
+	}
+
+}
