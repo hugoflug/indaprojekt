9e1eb15d7220e1080859f4cfcacd7f74042e7988
garlic <katja7667@gmail.com>
2012-04-26 11:45:17 +0200

    Fixed PowerUp generation/collision

--- src/indaprojekt/PowerUpGenerator.java
+++ src/indaprojekt/PowerUpGenerator.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import org.newdawn.slick.SlickException;
 
 public class PowerUpGenerator {
-	private final int MIN_TIME = 5000; // ms
+	private final int MIN_TIME = 2000; // ms
 	private final int MAX_TIME = 10000; // ms
 	
 	private Expirer expirer;
@@ -25,7 +25,7 @@ public class PowerUpGenerator {
 		PowerUp power = null;
 		if (expirer.hasExpired()) {
 			// TODO - the powerUps can end up anywhere, but if they hit an 
-			// obstacle, they will dissappear immediately, but will be 
+			// obstacle, they will disappear immediately, but will be 
 			// visible for 1/FPS seconds.
 			int choosePower = rand.nextInt(2);
 
