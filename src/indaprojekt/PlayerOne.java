fbc53f35c5c0e4bf1dee6720b901b2f8d3b59157
Hugo S <hugo.flugsvamp@gmail.com>
2012-04-27 13:17:58 +0200

    Removed old player images

--- src/indaprojekt/PlayerOne.java
+++ src/indaprojekt/PlayerOne.java
@@ -36,10 +36,10 @@ public class PlayerOne extends Player
 	{
 		Map<Direction, Animation> animMap = new HashMap<Direction, Animation>();
 		Color transp = Color.white;
-		animMap.put(Direction.UP, new Animation(new Image[]{new Image("res//images//up1.png",transp),new Image("res//images//up2.png",transp) }, 200));
-		animMap.put(Direction.RIGHT, new Animation(new Image[]{new Image("res//images//right1.png",transp),new Image("res//images//right2.png",transp)}, 200));
-		animMap.put(Direction.DOWN, new Animation(new Image[]{new Image("res//images//down1.png",transp), new Image("res//images//down2.png",transp)}, 200));
-		animMap.put(Direction.LEFT, new Animation(new Image[]{new Image("res//images//left1.png",transp), new Image("res//images//left2.png",transp)}, 200));
+		animMap.put(Direction.UP, new Animation(new Image[]{new Image("res//images//man1-up-leftFoot.png",transp),new Image("res//images//man1-up-rightFoot.png",transp) }, 200));
+		animMap.put(Direction.RIGHT, new Animation(new Image[]{new Image("res//images//man1-right-leftFoot.png",transp),new Image("res//images//man1-right-rightFoot.png",transp)}, 200));
+		animMap.put(Direction.DOWN, new Animation(new Image[]{new Image("res//images//man1-down-leftFoot.png",transp), new Image("res//images//man1-down-rightFoot.png",transp)}, 200));
+		animMap.put(Direction.LEFT, new Animation(new Image[]{new Image("res//images//man1-left-leftFoot.png",transp), new Image("res//images//man1-left-rightFoot.png",transp)}, 200));
 		
 		return animMap;
 	}
